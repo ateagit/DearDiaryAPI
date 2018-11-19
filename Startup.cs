fa412a4f3fa3795e5074da183a972f3ed029d1d2
Your Name <you@example.com>
2018-11-19 16:45:09 +1300

    Added Swagger for visualising API

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using DearDiaryLogs.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DearDiaryLogs
 {
@@ -31,6 +32,11 @@ namespace DearDiaryLogs
 
             services.AddDbContext<DearDiaryLogsContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("DearDiaryLogsContext")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Dear Diary API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +50,17 @@ namespace DearDiaryLogs
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
